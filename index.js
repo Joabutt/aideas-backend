e004a9e7c9a2a60ad850995a27d9692aa808c345
Joabutt <89288499+Joabutt@users.noreply.github.com>
2022-12-08 12:21:20 +0800

    Added new files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function yes(){
       }
     
     
-    const response = await axios.get(process.env.API_URL)
+    const response = await axios.get("https://api.aideas.dev/get")
     const array = response.data
 
 
